686c73ab8df877c208380ec1df909b580feb56b3
Jeremy Gulickson <jeremy.gulickson@me.com>
2017-04-12 12:21:48 -0400

    Minor Additions
    Minor Additions

--- Formatting-Functions.lua
+++ Formatting-Functions.lua
@@ -1,7 +1,7 @@
 ------------------------------------------Overview------------------------------------------
 -- Name:             Formatting Functions
 -- Notes:            Copyright (c) 2017 Jeremy Gulickson
--- Version:          1.0.03112017
+-- Version:          1.1.04122017
 -- Usage:            Functions to simplify common formatting operations.
 --
 -- Requirements:     FXTS (FXCM Trading Station)
@@ -9,8 +9,9 @@
 -- Documentation:    http://www.fxcodebase.com/bin/products/IndicoreSDK/3.3.0/help/Lua/web-content.htm
 --
 ---------------------------------------Version History--------------------------------------
--- v1.0.11062014:    Initial release
+-- v1.0.11062014:    -> Initial release
 --
+-- v1.1.04122017:    -> Added Format_Thousands()
 --------------------------------------------------------------------------------------------
 
 
@@ -51,6 +52,21 @@ function Format_Financial(aInput, aDecimals)
 	end
 	
 	return "$" .. zSign .. zBefore .. zResult .. zAfter;
+end
+
+
+function Format_Thousands(aInput, aDecimals)
+	-- Sourced from http://www.gammon.com.au/forum/?id=7805
+	aInput = string.format("%." .. aDecimals .. "f", aInput);
+	
+	local zResult = "";
+	local zSign, zBefore, zAfter = string.match(tostring(aInput), "^([%+%-]?)(%d*)(%.?.*)$")
+	while string.len(zBefore) > 3 do
+		zResult = "," .. string.sub(zBefore, -3, -1) .. zResult;
+		zBefore = string.sub(zBefore, 1, -4);
+	end
+	
+	return zSign .. zBefore .. zResult .. zAfter;
 end
 
 
